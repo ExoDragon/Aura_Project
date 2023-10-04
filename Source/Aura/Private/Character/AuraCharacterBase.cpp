37ba6a3210c8dea405a9c93c380e61adbd4de069
ExoDragon <to.terlaak@outlook.com>
2023-10-04 14:04:57 +0200

    Aura & Goblin CharacterBP

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -6,6 +6,10 @@
 AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
+
+	WeaponComponent = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	WeaponComponent->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	WeaponComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()
