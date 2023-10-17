b3cd23d778e6db4aa6363b2ac5addce88a119da6
ExoDragon <to.terlaak@outlook.com>
2023-10-17 17:39:11 +0200

    finished chapter 1,2 and 3 Section 5

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyrigth Exodragon
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyrigth Exodragon
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
